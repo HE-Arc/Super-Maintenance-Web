8d1fa71367d34da966207e985723673c53387309
sebastie.roso <sebastien.roso@he-arc.ch>
2020-10-23 11:36:02 +0200

    first migration file

---
+++ database/migrations/2020_10_23_092945_create_task.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTask extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->comment();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}

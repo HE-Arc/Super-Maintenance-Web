696cec0ab19a65a3a587b920ab117796b20165e3
sebastie.roso <sebastien.roso@he-arc.ch>
2020-11-05 11:46:45 +0100

    create WebService Task and update route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+use DB;
+
+class TaskController extends Controller{
+
+	public function getTaskByMachineId(Request $request, $id_machine)
+	{
+		$tasks = DB::table('tasks')->where('id_machine', $request->input('id_machine'))->get();
+
+		$response["tasks"] = $tasks;
+		$response["success"] = 1;
+
+		return response()->json($response);
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\MaintainerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('maintainer','MaintainerController@createMaintainer');   //for creating maintainer
-Route::get('maintainer/{id}','MaintainerController@updateMaintainer'); //for updating maintainer
-Route::post('maintainer/{id}','MaintainerController@deleteMaintainer');  // for deleting maintainer
-Route::get('maintainers','MaintainerController@index'); // for retrieving maintainers
+Route::post('maintainer',[MaintainerController::class], 'createMaintainer');   //for creating maintainer
+Route::get('maintainer/{id}',[MaintainerController::class], 'updateMaintainer'); //for updating maintainer
+Route::post('maintainer/{id}',[MaintainerController::class], 'deleteMaintainer');  // for deleting maintainer
+Route::get('maintainers',[MaintainerController::class], 'index'); // for retrieving maintainers
+
+Route::get('tasks/{id_machine}', [TaskController::class, 'getTaskByMachineId']); // for retrieving all task of machine
+
+

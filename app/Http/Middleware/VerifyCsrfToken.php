a2141e62485bb3239f988b3c4e3bbaa2793d130f
sebastie.roso <sebastien.roso@he-arc.ch>
2020-11-12 18:47:47 +0100

    update maintainer crud & maintain update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         "maintainer",
         "maintainer/*", 
+        "maintainer_delete/*",
         
         "maintain",
         "maintain/*",

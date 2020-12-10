cec4b9a9edba7672b6a1715d6f138b4137480719
Alan.chambaz <alan.chambaz@he-arc.ch>
2020-12-10 11:32:44 +0100

    Add logo

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -7,6 +7,13 @@
       dark
       style="z-index:999998 !important;"
     >
+      <img
+        height="50px"
+        width="50px"
+        src="../../assets/smw_logo.png"
+        class="mr-5"
+      >
+
       <v-toolbar-title>Super maintenance web</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>

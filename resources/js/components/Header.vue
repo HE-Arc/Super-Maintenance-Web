e2e059f7fe600346d48f9c74fb13d37fe9230690
colin.pelletie <colin.pelletier@he-arc.ch>
2020-12-17 21:31:39 +0100

    Add a link

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -7,13 +7,14 @@
       dark
       style="z-index:999998 !important;"
     >
-      <img
-        height="50px"
-        width="50px"
-        src="../../assets/smw_logo.png"
-        class="mr-5"
-      >
-
+      <a href="/">
+        <img
+          height="50px"
+          width="50px"
+          src="../../assets/smw_logo.png"
+          class="mr-5"
+        >
+      </a>
       <v-toolbar-title>Super maintenance web</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
